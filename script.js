9fd62b45dbbae497fd51c5e8899dfec4a7e8573b
Remy <remy@chaoticmewtral.com>
2022-10-13 12:50:41 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function add() {
-
+function add(a, b) {
+    return a + b;
 }
 
-function subtract() {
-
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide() {
-    
+function divide(a, b) {
+    return a / b;
 }
