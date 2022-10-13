aa77aad907652d6f2180c36e2b60b2d9c84b016d
Remy <remy@chaoticmewtral.com>
2022-10-13 13:19:07 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    let result;
+    switch(operator) {
+        case "add":
+            result = add(a, b);
+            break;
+        case "subtract":
+            result = subtract(a, b);
+            break;
+        case "multiply":
+            result = multiply(a, b);
+            break;
+        case "divide":
+            result = divide(a, b);
+    }
+    return result;
+}
