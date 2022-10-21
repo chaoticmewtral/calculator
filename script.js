34fa1dacd432db5eb680c841e8b561c47fadc9c7
Remy <remy@chaoticmewtral.com>
2022-10-21 15:40:26 -0400

    Add to script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-/*
+let aValue = '';
+let bValue = '';
+let currentOperator = '';
+let resetDisplay = true;
 
-*/
+const numbers = document.querySelectorAll('[.num]');
+const operators = document.querySelectorAll('[.ops]');
+const decimalButton = document.getElementById('dec');
+const deleteButton = document.getElementById('delete');
+const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
+
+const mainDisplay = document.getElementById('display');
+const tapeDisplay = document.getElementById('tape');
+
